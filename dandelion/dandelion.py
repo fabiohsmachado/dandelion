7cf7df9c57cded9a1e679b20167df729ee4ca058
Fabio Machado <fabiohsmachado@gmail.com>
2017-03-15 20:54:45 -0300

    Initial definition of the Dandelion class

--- dandelion/dandelion.py
+++ dandelion/dandelion.py
@@ -1,4 +1,16 @@
-"""Provides a command line interface for the dandelion coding and decoding modules"""
+"""Defines the Dandelion class"""
+
+class Dandelion:
+    """The Dandelion code"""
+
+    # The code itself. Each i-th element of the array is a set containing the
+    # parent of the i-th node and its edge label in the characteristic tree.
+    code = []
+
+    def __init__(self, code=None):
+        """Initializes the code. An already existing code can be passed too."""
+        if code is not None:
+            self.code = code;
 
 def main():
     """Parses command line arguments"""
