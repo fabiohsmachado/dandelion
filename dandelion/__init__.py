d56133350a92e5308cd6c13edaf7485e0ba07da3
Fabio Machado <fabiohsmachado@gmail.com>
2017-03-15 20:36:38 -0300

    First commit, project's skeleton

---
+++ dandelion/__init__.py
@@ -0,0 +1,11 @@
+"""A bijective linear time coding and decoding for k-trees
+
+This package presents a method for coding and decoding k-Trees from strings
+called Generalized Dandelion Code.
+
+It is an implemetentation of the algorithm describe in Camintini's et. al.
+paper
+[[https://www.researchgate.net/publication/225969891_Bijective_Linear_Time_Coding_and_Decoding_for_k-Trees][Bijective
+Linear Coding and Decoding for k-Trees]].
+
+"""
---
+++ dandelion/dandelion.py
@@ -0,0 +1,8 @@
+"""Provides a command line interface for the dandelion coding and decoding modules"""
+
+def main():
+    """Parses command line arguments"""
+    return None
+
+if __name__ == "__main__":
+    main()
