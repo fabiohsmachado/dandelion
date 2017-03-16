5a510b2ccae04e740f6e7d4122e5e4d6c16c6a8a
Fabio Machado <fabiohsmachado@gmail.com>
2017-03-15 21:53:24 -0300

    Dandelion Code validation

--- dandelion/dandelion.py
+++ dandelion/dandelion.py
@@ -5,12 +5,34 @@ class Dandelion:
 
     # The code itself. Each i-th element of the array is a set containing the
     # parent of the i-th node and its edge label in the characteristic tree.
-    code = []
+    code = [];
+
+    #The number of variables in the k-Tree
+    N = None;
+
+    def validateCode(code):
+        """Check if the code is well formed"""
+
+        #Assert types
+        assert isinstance(code, list);
+        for t in code:
+            assert isinstance(t, tuple);
+
+        #Assert number of elements according to N
+        assert len(code) == self.N - 2
+
+        return True;
 
     def __init__(self, code=None):
         """Initializes the code. An already existing code can be passed too."""
         if code is not None:
+            try:
+                self.validateCode(code);
+            except AssertionError:
+                return "Invalid code";
+
             self.code = code;
+            self.N = len(code) + 2;
 
 def main():
     """Parses command line arguments"""
