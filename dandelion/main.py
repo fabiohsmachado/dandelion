0ef13d13d268f406b76e26789c5c65489cc000df
Fabio Machado <fabiohsmachado@gmail.com>
2017-03-18 03:09:54 -0300

    Added the kTree attribute

--- dandelion/main.py
+++ dandelion/main.py
@@ -1,16 +1,20 @@
 """Defines the Dandelion class"""
+import networkx as nx;
 
 class Dandelion:
     """The Dandelion code"""
 
-    def __init__(self, N, k, initialCode=None):
+    def __init__(self, N, k, code=None, kTree=None):
         """Initializes the code."""
 
         self.N = N;
         self.k = k;
         self._code = None;
-        if initialCode is not None:
-            self.code = initialCode;
+        self._kTree = None;
+        if code is not None:
+            self.code = code;
+        if kTree is not None:
+            self.kTree = kTree;
 
     # The code itself. Each i-th element of the array is a tuple containing the
     # parent of the i-th node and its edge label in the characteristic tree.
@@ -22,6 +26,15 @@ class Dandelion:
     def code(self, value):
         self.validateCode(value);
         self._code = value;
+
+    @property
+    def kTree(self):
+        return self._kTree;
+
+    @kTree.setter
+    def kTree(self, value):
+        self.validateKTree(value);
+        self._kTree = value;
 
     def validateCode(self, code):
         """Check if the code is well formed.
@@ -37,6 +50,17 @@ class Dandelion:
         #Assert number of elements according to N and k
         assert len(code) == self.N - self.k - 2
 
+        return True;
+
+    def validateKTree(self, kTree):
+        """Check if the kTree is correct.
+
+        DOES NOT validates the kTree structure, instead does minimum validation
+        to check if it an instance of networkx's Graph.
+
+        """
+
+        assert isinstance(kTree, nx.Graph)
         return True;
 
 def main():
--- dandelion/tests/test_dandelion.py
+++ dandelion/tests/test_dandelion.py
@@ -1,4 +1,7 @@
 import pytest
+
+import networkx as nx
+import numpy as np
 from ..main import Dandelion
 
 class TestDandelion():
@@ -7,6 +10,18 @@ class TestDandelion():
     N, k = 11, 3;
     good_code = [(0, -1), (2, 1), (8, 3), (8, 2), (1, 3), (5, 3)];
     bad_code = [(0, -1), (0, 2), 4, [1, 3]]
+
+    kTree = nx.Graph();
+    kTree.add_nodes_from([i for i in np.arange(N)]);
+    kTree.add_edges_from([(1,2),(1,8),(1,5),(1,7),
+                          (2,5),(2,6),(2,8),(2,3),(2,11),(2,9),(2,10),
+                          (3,8),(3,5),(3,9),(3,4),(3,10),(3,11),
+                          (4,9),(4,11),
+                          (5,7),(5,8),
+                          (6,9),(6,8),
+                          (7,8),
+                          (8,9),
+                          (9,10),(9,11)])
 
     def test_dandelion_constructs_good_code(self):
         assert isinstance(Dandelion(11, 3, self.good_code), Dandelion);
@@ -35,3 +50,12 @@ class TestDandelion():
     def test_request_empty_code_returns_none(self):
         DCode = Dandelion(11, 3);
         assert DCode.code == None;
+
+    def test_dandelion_constructs_from_kTree(self):
+        DCode = Dandelion(11, 3, kTree=self.kTree);
+        assert DCode.kTree == self.kTree;
+
+    def test_dandelion_validates_bad_kTree(self):
+        DCode = Dandelion(11, 3);
+        with pytest.raises(AssertionError):
+            DCode.kTree = [11, 5] #Not an instance of nx.Graph;
