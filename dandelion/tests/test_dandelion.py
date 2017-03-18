07386d0dd4a983e696e02f144273cb54ca46eff0
Fabio Machado <fabiohsmachado@gmail.com>
2017-03-18 00:50:44 -0300

    Added code as a property (so it can be validated before set)

--- dandelion/tests/test_dandelion.py
+++ dandelion/tests/test_dandelion.py
@@ -1,23 +1,37 @@
 import pytest
-from .. import dandelion
+from ..main import Dandelion
 
 class TestDandelion():
     """Tests for the Dandelion code"""
 
-    code = [(0,-1), (0, -1), (2, 1), (8, 3), (8, 2), (1, 3), (5, 3)];
+    good_code = [(0,-1), (0, -1), (2, 1), (8, 3), (8, 2), (1, 3), (5, 3)];
+    bad_code = [(0, -1), (0, 2), 4, [1, 3]]
 
-    def test_dandelion_constructor_good_code(self):
-        assert isinstance(dandelion.Dandelion(self.code), dandelion.Dandelion)
+    def test_dandelion_constructs_good_code(self):
+        assert isinstance(Dandelion(self.good_code), Dandelion);
 
     def test_dandelion_validates_bad_code_structure(self):
-        code = [(0, -1), (0, 2), [1, 3]]
         with pytest.raises(AssertionError):
-            dandelion.Dandelion(code)
+            Dandelion(self.bad_code);
 
     def test_dandelion_validates_bad_code_length(self):
-        codeString = [(0, -1), (0, 2)]
-        code = dandelion.Dandelion()
-        code.N = 5;
+        DCode = Dandelion();
+        DCode.N = 5;
 
+        codeString = [(0, -1), (0, 2)]
         with pytest.raises(AssertionError):
-            code.validateCode(codeString)
+            DCode.validateCode(codeString);
+
+    def test_assigning_new_code_validates_it(self):
+        DCode = Dandelion();
+        with pytest.raises(AssertionError):
+            DCode.code = self.bad_code;
+
+    def test_requesting_code_returns_it(self):
+        DCode = Dandelion();
+        DCode.code = self.good_code;
+        assert DCode.code == self.good_code;
+
+    def test_request_empty_code_returns_none(self):
+        DCode = Dandelion();
+        assert DCode.code == None;
